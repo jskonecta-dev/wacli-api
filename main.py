29cc12813a7a455d54175c70d84d63cce95f0987
jskonecta-dev <jskonecta@gmail.com>
2026-09-25 15:27:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,6 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# extraer datos para el csv
-
 def extraer_datos_soporte(local_path):
     try:
         img = Image.open(local_path)
